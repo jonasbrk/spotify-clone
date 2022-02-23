3c2eab7ed4779c9e104820561d15bd33f25930a2
João Pedro <jonas0378@gmail.com>
2022-02-23 00:01:27 -0300

    chore: folder structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className='navBtnWrapper'></div>
+        </div>
+    );
+};
+
+export default Header;
