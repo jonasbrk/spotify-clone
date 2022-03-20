c3ecbf33ea511ae2c31dd24ea4439a8701a30f2e
João Pedro <jonas0378@gmail.com>
2022-03-20 15:47:51 -0300

    feat: header navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.styles.css';
 import { Button, UserMenu } from '../index';
 import { ArrowLeftImg, ArrowRigthImg } from '../../assets/svg/index';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const navigateTo = (index) => {
+    if (location.pathname == '/' && index == -1) return;
+    navigate(index);
+  };
   return (
     <header>
       <div className="header__wrapper">
-        <Button type="icon" src={<ArrowLeftImg />} effect="grow" />
-        <Button type="icon" src={<ArrowRigthImg />} />
+        <Button
+          type="icon"
+          src={<ArrowLeftImg />}
+          onClick={() => {
+            navigateTo(-1);
+          }}
+        />
+        <Button
+          type="icon"
+          src={<ArrowRigthImg />}
+          onClick={() => {
+            navigateTo(+1);
+          }}
+        />
       </div>
       <div className="header__wrapper">
         <UserMenu />
