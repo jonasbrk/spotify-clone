c4a078a6db60ccc2efe008e5cc63e8b530510baf
João Pedro <jonas0378@gmail.com>
2022-03-28 18:02:12 -0300

    feat: options menu component

---
+++ src/assets/svg/Options.img.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const OptionsImg = () => {
+  return (
+    <svg
+      role="img"
+      height="32"
+      width="32"
+      viewBox="0 0 24 24"
+      className="Svg-sc-1bi12j5-0 hDgDGI"
+    >
+      <path d="M4.5 13.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm15 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm-7.5 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+    </svg>
+  );
+};
