703d0784d1d2b6201116e51bbe3c4ef0c9b34c94
João Pedro <jonas0378@gmail.com>
2022-03-29 09:38:00 -0300

    chore: refactoring button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Button.styles.css';
 
 export const Button = (props) => {
@@ -7,46 +7,26 @@ export const Button = (props) => {
 
   return (
     <>
-      {type === 'nav' && to && (
-        <Link to={to} className={`button--nav ${custom ? custom : ''}`}>
-          {src && <div className="button__icon">{src}</div>}
-          {children && <span className="button__title">{children}</span>}
-        </Link>
-      )}
-      {type === 'nav' && onClick && (
-        <button
-          onClick={onClick}
-          className={`button--nav ${custom ? custom : ''}`}
+      {to && (
+        <NavLink
+          to={to}
+          className={({ isActive }) =>
+            `button--${type} ${isActive && 'button--active'} ${
+              custom ? custom : ''
+            }`
+          }
         >
           {src && <div className="button__icon">{src}</div>}
           {children && <span className="button__title">{children}</span>}
-        </button>
+        </NavLink>
       )}
-
-      {type === 'login' && (
-        <a onClick={onClick} className={`button--nav ${custom ? custom : ''}`}>
-          <div className="button__icon">{src}</div>
+      {onClick && (
+        <button
+          onClick={onClick}
+          className={`button--${type} ${custom ? custom : ''}`}
+        >
+          {src && <div className="button__icon">{src}</div>}
           {children && <span className="button__title">{children}</span>}
-        </a>
-      )}
-
-      {type === 'icon' && (
-        <button
-          href="#"
-          onClick={onClick}
-          className={`button--icon ${custom ? custom : ''}`}
-        >
-          <div className="button__icon">{src}</div>
-        </button>
-      )}
-
-      {type === 'player' && (
-        <button
-          href="#"
-          onClick={onClick}
-          className={`button--player ${custom ? custom : ''}`}
-        >
-          <div className="button__icon">{src}</div>
         </button>
       )}
     </>
